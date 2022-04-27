29785bcc1b2126cdafe7fc87f44e577cf43320c8
beshoyfayz201 <beshoyfayz201@gmail.com>
2022-04-27 20:02:13 +0200

    responsivness

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,40 @@ class _HomePageState extends State<HomePage> {
   int turn = 0;
   String activePlayer = "X";
   bool gameMode = false;
+  List board() => [
+        SizedBox(
+          child: SwitchListTile.adaptive(
+              title: Text("single  mode "),
+              value: gameMode,
+              onChanged: (v) {
+                setState(() {
+                  gameMode = v;
+                });
+              }),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ResultBoard(
+              player: "X",
+              Activeplayer: activePlayer,
+            ),
+            Text(Player.xScore.toString() + " : " + Player.oScore.toString(),
+                style: TextStyle(
+                    color: Color.fromARGB(255, 10, 250, 226),
+                    fontFamily: "ru",
+                    fontSize: 80,
+                    shadows: [
+                      Shadow(color: Colors.cyan.shade50, blurRadius: 2)
+                    ])),
+            ResultBoard(
+              player: "O",
+              Activeplayer: activePlayer,
+            )
+          ],
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
     if (game.winner == "x") {
@@ -41,120 +75,96 @@ class _HomePageState extends State<HomePage> {
             image: DecorationImage(
                 image: AssetImage("assets/images/x.jpg"), fit: BoxFit.cover)),
         child: SafeArea(
-          child: Column(
-            children: [
-              SwitchListTile.adaptive(
-                  title: Text("single player mode "),
-                  value: gameMode,
-                  onChanged: (v) {
-                    setState(() {
-                      gameMode = v;
-                    });
-                  }),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ResultBoard(
-                    player: "X",
-                    Activeplayer: activePlayer,
-                  ),
-                  Text(
-                      Player.xScore.toString() +
-                          " : " +
-                          Player.oScore.toString(),
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 10, 250, 226),
-                          fontFamily: "ru",
-                          fontSize: 80,
-                          shadows: [
-                            Shadow(color: Colors.cyan.shade50, blurRadius: 2)
-                          ])),
-                  ResultBoard(
-                    player: "O",
-                    Activeplayer: activePlayer,
+            child: MediaQuery.of(context).orientation == Orientation.portrait
+                ? Column(
+                    children: [...board(), xo(), restart()],
                   )
-                ],
-              ),
-              (gameOver ||
-                      game.winner != " " ||
-                      Player.oIndex.length + Player.xIndex.length == 9)
-                  ? Center(
-                      child: ResultBoard(
-                          player:
-                              " (${activePlayer = activePlayer == "X" ? "O" : "X"}_$activePlayer)\n  won",
-                          Activeplayer: activePlayer),
-                    )
-                  : Expanded(
-                      child: GridView.count(
-                        crossAxisCount: 3,
-                        children: List.generate(
-                            9,
-                            (index) => GestureDetector(
-                                  onTap: gameOver
-                                      ? null
-                                      : () async {
-                                          print(game.winner);
-                                          if (!Player.oIndex.contains(index) &&
-                                              !Player.xIndex.contains(index)) {
-                                            print("$index------");
-                                            setState(() {
-                                              game().play(index, activePlayer,
-                                                  gameMode);
-
-                                              activePlayer = activePlayer == "X"
-                                                  ? "O"
-                                                  : "X";
-                                              if (gameMode &&
-                                                  !(Player.oIndex.length +
-                                                          Player.xIndex.length >
-                                                      8)) {
-                                                game().autoPlaye(activePlayer);
-                                                activePlayer =
-                                                    activePlayer == "X"
-                                                        ? "O"
-                                                        : "X";
-                                              }
-                                            });
-                                          }
-                                        },
-                                  child: Card(
-                                    color: Colors.white24,
-                                    child: Center(
-                                      child: Text(
-                                        setMark(index),
-                                        style: TextStyle(
-                                            color: Player.xIndex.contains(index)
-                                                ? Colors.blueAccent.shade700
-                                                : Colors.deepOrange,
-                                            fontSize: 45,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-                                  ),
-                                )),
+                : Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [...board(), restart()],
+                        ),
                       ),
-                    ),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(primary: Colors.white10),
-                icon: Icon(Icons.replay),
-                onPressed: () {
-                  setState(() {
-                    gameOver = false;
-                    turn = 0;
-                    activePlayer = "X";
-                    game.winner = " ";
-                    Player.oIndex = [];
-                    Player.xIndex = [];
-
-                    gameMode = false;
-                  });
-                },
-                label: Text("Reppeat the game"),
-              )
-            ],
-          ),
-        ),
+                      xo(),
+                    ],
+                  )),
       ),
+    );
+  }
+
+  xo() => (gameOver ||
+          game.winner != " " ||
+          Player.oIndex.length + Player.xIndex.length == 9)
+      ? Center(
+          child: ResultBoard(
+              player:
+                  " (${activePlayer = activePlayer == "X" ? "O" : "X"}_$activePlayer)\n  won",
+              Activeplayer: activePlayer),
+        )
+      : Expanded(
+          child: GridView.count(
+            crossAxisCount: 3,
+            children: List.generate(
+                9,
+                (index) => GestureDetector(
+                      onTap: gameOver
+                          ? null
+                          : () async {
+                              print(game.winner);
+                              if (!Player.oIndex.contains(index) &&
+                                  !Player.xIndex.contains(index)) {
+                                print("$index------");
+                                setState(() {
+                                  game().play(index, activePlayer, gameMode);
+
+                                  activePlayer =
+                                      activePlayer == "X" ? "O" : "X";
+                                  if (gameMode &&
+                                      !(Player.oIndex.length +
+                                              Player.xIndex.length >
+                                          8)) {
+                                    game().autoPlaye(activePlayer);
+                                    activePlayer =
+                                        activePlayer == "X" ? "O" : "X";
+                                  }
+                                });
+                              }
+                            },
+                      child: Card(
+                        color: Colors.white24,
+                        child: Center(
+                          child: Text(
+                            setMark(index),
+                            style: TextStyle(
+                                color: Player.xIndex.contains(index)
+                                    ? Colors.blueAccent.shade700
+                                    : Colors.deepOrange,
+                                fontSize: 45,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    )),
+          ),
+        );
+  ElevatedButton restart() {
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(primary: Colors.white10),
+      icon: Icon(Icons.replay),
+      onPressed: () {
+        setState(() {
+          gameOver = false;
+          turn = 0;
+          activePlayer = "X";
+          game.winner = " ";
+          Player.oIndex = [];
+          Player.xIndex = [];
+
+          gameMode = false;
+        });
+      },
+      label: Text("Reppeat the game"),
     );
   }
 
